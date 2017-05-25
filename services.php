777421267aacc1d28c9cddd565de864a155a38e5
sayederfanarefin <erfanjordison@gmail.com>
2017-05-25 22:46:46 +0600

    asdf

---
+++ services.php
@@ -0,0 +1,241 @@
+<!DOCTYPE HTML>
+<!--
+	Greatness by FreeHTML5.co
+	Twitter: http://twitter.com/fh5co
+	URL: http://FreeHTML5.co
+-->
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Our Services</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+	
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+<?php require_once("nav.php");?> 
+
+	<header id="gtco-header" class="gtco-cover gtco-cover-sm" role="banner" style="background-image:url(images/ser1.jpg);">
+		<div class="overlay"></div>
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Services</h1>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<div class="gtco-section">
+		<div class="gtco-container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+					Thai Airside Aviation is committed to provide customized cost effective engineering services for all international airlines operating to/from Bangladesh. Listed below the expertise and a comprehensive list of services we will offer to our clients:
+
+				</div>
+
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+					<h2>Technical Support</h2>
+
+					Our team consists of highly skilled and dedicated professionals, trained on various modern days’ Aircrafts & Engines in Line and Base maintenance. 
+<br>
+<br>
+Technical handling services provided by Airside Engineering as follows: 
+
+				</div>
+
+<div class="col-md-8 col-md-offset-2 gtco-heading">
+					
+<ul>
+
+
+
+					  <li>Aircraft Inspections </li>
+					  <li>Pre-flight Inspection & Transit Check with airworthiness release certification </li>
+					  <li>Daily Checks</li>
+					  <li>Fault isolation and defect rectification of various aircraft and power plant systems including IFE</li>
+					  <li>Modifications & service bulletins related to line maintenance only </li>
+					  <li>AOG Aircraft recoveries</li>
+					  <li>Suitable storage space for aircraft wheels, spare parts and special equipment</li>
+					  <li>Other Engineering Support</li>
+					  <li>24 Hour Technical Support</li>
+					</ul> 
+
+				</div>
+
+			</div>
+			
+		</div>
+	</div>
+
+	<div class="gtco-cover gtco-cover-sm" style="background-image:url(images/ser2.jpg);">
+		<div class="overlay"></div>
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0 text-center">
+					<div class="display-t">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="gtco-team" class="gtco-section">
+		<div class="gtco-container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+				<b>
+				We are committed to maintaining quality and integrity in all aspects of our operation and assisting you, our valued customer, in making informed choices to meet your flying needs. Besides Line Maintenance Airside Capable to provide following services:
+</b>
+
+				</div>
+			</div>
+			<div class="row row-pb-md">
+			<div class="col-md-1"></div>
+				<div class="col-md-10 animate-box" data-animate-effect="fadeIn">
+					<div class="gtco-staff">
+						
+						<h3>Ground Handling Services</h3>
+						<p style="text-align: justify;
+    text-justify: inter-word;">Thai Airside Aviation wishes a reputation for providing first-rate above-the-wing ground handling services for some of the world’s leading airlines that operates in HSIA.
+						<br>
+						<br>
+We wish to provide reliabl e on-demand ground handling services with speed, efficiency, excellence and accuracy. 
+						<br>
+<br>
+From ramp handling, VIP, passenger, cargo and luggage handling, hotel accommodation, meet and assist, catering, transportation, customs and immigration assistance, we wish to take care of all the flight requirements.
+</p>
+						
+					</div>
+				</div>
+
+				</div>
+
+				<div class="row row-pb-md">
+
+				<div class="col-md-1 animate-box"></div>
+
+				
+				<div class="col-md-5 animate-box" data-animate-effect="fadeIn">
+					<div class="gtco-staff">
+						
+						<h3>Flight Planning</h3>
+						<p style="text-align: justify;
+    text-justify: inter-word;">Our teams of professional and certified dispatchers use state-of-the-art flight planning systems to provide reliable and accurate flight plans that work in compliance with all stakeholders involved.
+Our own in-house dispatchers customize flight plans and tailor your needs by combining different flight planning systems that give the most optimum flight plans. 
+</p>
+
+<ul style="text-align: justify;
+    text-justify: inter-word;">
+
+
+
+					  <li>Flight Crew Briefings</li>
+					  <li>ATC Filings</li>
+					  <li>Test Plans</li>
+					  <li>Route Analysis</li>
+					  <li>Data Uplink</li>
+					  <li>Flight Watch</li>
+
+					  </ul>
+						
+					</div>
+				</div>
+				<div class="col-md-5 animate-box" data-animate-effect="fadeIn">
+					<div class="gtco-staff">
+						
+						<h3>Flight Permits</h3>
+						<p style="text-align: justify;
+    text-justify: inter-word;">We have the ability to secure all required flight permits in the fastest and most efficient way.
+We look after overflight and landing clearances for a wide range of flight types, from commercial airlines to VIP flight operators and emergency flights, anywhere in the world, in the shortest time, and at competitive prices.
+</p>
+						
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+	
+
+
+<?php require_once("footer.php");?> 
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
